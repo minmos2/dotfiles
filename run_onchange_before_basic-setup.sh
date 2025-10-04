325cbf51d9e071c4c2bdf2fe46bbb74b10722468
minmos2 <93261594+minmos2@users.noreply.github.com>
2025-10-04 21:41:01 +0200

    Add files via upload

---
+++ run_onchange_before_basic-setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo dnf install btop neovim bat asciiquarium alacritty fastfetch ranger fzf
---
+++ run_onchange_install_nerdfonts.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+declare -a fonts=(
+#	Agave
+#	AnonymousPro
+#	Arimo
+#	AurulentSansMono
+#	BigBlueTerminal
+#	BitstreamVeraSansMono
+#	CascaidaCode
+#	CodeNewRoman
+#	Cousine
+#	DaddyTimeMono
+#	DejaVuSansMono
+#	DroidSansMono
+	FantasqueSansMono
+#	FiraCode
+#	FiraMono
+#	Go-Mono
+	Hack
+#	Gohu
+#	Hasklig
+#	HeavyData
+#	Hermit
+#	iA-Writer
+#	IBMPlexMono
+#	Inconsolate
+#	InconsolataGo
+#	InconsolataLGC
+#	Iosevka
+#	JetBrainsMono
+#	Lekton
+#	LiberationMono
+#	Lilex
+#	Meslo
+#	Monofur
+#	Mononoki
+#	Monoid
+#	MPlus
+#	NerdFontsSymbolsOnly
+#	Noto
+#	OpenDyslexic
+#	Overpass
+#	ProFont
+#	ProggyClean
+#	RobotoMono
+#	ShareTechMono
+#	Terminus
+#	Tinos
+#	Ubuntu
+#	UbuntuMono
+#	VictorMono
+#	Block_comment
+)
+
+version='2.2.2'
+fonts_dir="${HOME}/.local/share/fonts"
+
+if [[ ! -d "$fonts_dir" ]]; then
+    mkdir -p "$fonts_dir"
+fi
+
+for font in "${fonts[@]}"; do
+    zip_file="${font}.zip"
+    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+    echo "Downloading $download_url -O"
+    wget "$download_url"
+    unzip "$zip_file" -d "$fonts_dir" -x "*.txt/*" -x "*.md/*"
+    rm "$zip_file"
+done
+
+find "$fonts_dir" -name '*Windows Compatible*' -delete
+
+fc-cache -fv
+
