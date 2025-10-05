bd6c2e7169c87b9ce9c49d3f657ce0abdcaa604e
Moritz Becker <minmos@minmos.xyz>
2025-10-05 13:38:02 +0200

    added codecs, some flatpaks, commented out ohmyzsh install

--- run_onchange_after_install_terminal_tools.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo dnf install zsh 
-chsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
--- run_onchange_before_basic-setup.sh
+++ run_onchange_before_basic-setup.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
-sudo dnf install btop neovim bat asciiquarium alacritty fastfetch ranger fzf python3
+sudo dnf remove akregator kolourpaint kpat kadressbook kmail kmouth neochat
+sudo dnf install btop neovim bat asciiquarium alacritty fastfetch ranger fzf python3 xournalpp git make gcc 
 
 #flatpak
 flatpak install flathub com.nextcloud.desktopclient.nextcloud
-flatpak install flathub com.bitwarden.desktop
-flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.bitwarden.desktop 
+flatpak install flathub com.discordapp.Discord  
+flatpak install flathub org.signal.Signal
+flatpak install flathub org.videolan.VLC
+flatpak install flathub it.mijorus.gearlever
 
 #netbird
 curl -fsSL https://pkgs.netbird.io/install.sh | sh
 
+#vscode (commands from vscode website)
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+dnf check-update
+sudo dnf install code
+
+#install codecs
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install libavcodec-freeworld
